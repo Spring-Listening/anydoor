8bdd4ecffc09f7bbfcc00aec3821b1141188159d
zoucw <zoucw@yunjiglobal.com>
2021-08-19 17:03:46 +0800

    init

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@
  * @Author: chunwen (chunwen.zou@caibeitv.com)
  * @Date: 2021-08-19 16:19:04
  * @LastEditors: Please set LastEditors
- * @LastEditTime: 2021-08-19 16:56:50
+ * @LastEditTime: 2021-08-19 17:02:54
  */
 console.log(22222)
-console.log(test)
+console.log('test')
